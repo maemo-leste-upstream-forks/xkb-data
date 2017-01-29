32fb8851eb043ee591fa75e2613ee9ab251e24b3
Mihail Konev <k.mvc@ya.ru>
2017-01-30 08:46:17 +1000

    autogen: add default patch prefix
    Signed-off-by: Mihail Konev <k.mvc@ya.ru>
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- autogen.sh
+++ autogen.sh
@@ -9,6 +9,9 @@ cd "$srcdir"
 autopoint --force
 AUTOPOINT='intltoolize --automake --copy' autoreconf -v --install --force || exit 1
 
+git config --local --get format.subjectPrefix >/dev/null 2>&1 ||
+    git config --local format.subjectPrefix "PATCH xkeyboard-config"
+
 cd "$ORIGDIR" || exit $?
 if test -z "$NOCONFIGURE"; then
     exec "$srcdir"/configure "$@"
